28b245f72992fc9c4238e444cf99da39574e0b22
Inigo <igponce@gmail.com>
2021-10-04 17:44:49 +0200

    Tests simple transfer

---
+++ tests/test_erc721_spec.py
@@ -0,0 +1,71 @@
+#!/usr/env python
+#
+# Test ERC721 specification (minimal)
+
+import pytest
+import logging
+import web3
+from brownie import accounts, SimpleERC721 , exceptions, reverts
+
+@pytest.fixture
+def token():
+    """
+    Deploy the contract and mint NFTs owned by accounts[0]
+    """
+    erc721 = accounts[0].deploy(SimpleERC721)
+    return erc721
+
+
+def test_constructor():
+    """
+    Tests the constructor - accounts[0] deploys the contract.
+    ERC721 spec states that it is possible _not_ to emit transfer
+    events during contract creation.
+    
+    In our case, all minted tokens are owner by the deployer address.
+    """
+
+    deployer = accounts[0]
+
+    token = deployer.deploy(SimpleERC721)
+
+    assert token.balanceOf(deployer.address) > 0
+
+def test_simple_transfers(token):
+    """
+    ERC721 transfers are tricky.
+    There are 3 ways to transfer tokens:
+      - A (human?) person does the transfer.
+      - A contract transfer a token 
+          - ... and it has permission to transfer just one token.
+          - ... or it cat transfer any token this account has.
+          
+    """
+
+    sender = accounts[0].address
+    receiver = accounts[1].address
+    tokenid = 1
+
+    # Transfer to the zero address verboten
+    with reverts():
+       token.transferFrom(receiver, 
+            '0x0000000000000000000000000000000000000000',
+            tokenid, {'from': receiver})
+
+    
+    initial_balance = token.balanceOf(receiver)
+
+    tx = token.transferFrom(sender, receiver, tokenid)
+    assert token.balanceOf(receiver) == 1 + initial_balance
+
+    transfer_event = dict(tx.events).get('Transfer')
+
+    assert (transfer_event['_from'] == sender) & (
+            transfer_event['_to'] == receiver ) & ( 
+            transfer_event['_tokenid'] == tokenid)
+
+    with reverts():
+        # Only owner can transfer tokens
+        token.transferFrom(receiver, sender, tokenid, {'from': sender})
+        # Sending to the null address is not alloweD
+
